ed81450df783870902886b933abb67608eba9c72
Brook Humphrey <webmedic@gmail.com>
2015-04-17 09:01:34 -0700

    added python 3 version of chardet

---
+++ src/chardet/__init__.py
@@ -0,0 +1,33 @@
+######################## BEGIN LICENSE BLOCK ########################
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+# 02110-1301  USA
+######################### END LICENSE BLOCK #########################
+
+
+from .compat import PY2, PY3
+from .universaldetector import UniversalDetector
+from .version import __version__, VERSION
+
+
+def detect(byte_str):
+    if (PY2 and isinstance(byte_str, str)) or (PY3 and
+                                               not isinstance(byte_str,
+                                                              bytes)):
+        raise ValueError('Expected a bytes object, not a unicode object')
+
+    u = UniversalDetector()
+    u.feed(byte_str)
+    u.close()
+    return u.result
